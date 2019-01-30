823230a2193088959164a8fa94c969aff03a7589
Broadcom SDK Release <sdk.releases@broadcom.com>
2019-01-30 02:18:15 -0800

    SDK-166509: Use share memory mechanism to speed up soc_cmicx_led_link_status ...
    Devices: 56870_A0
    Module: LED
    
    Use share memory mechanism to speed up soc_cmicx_led_link_status and
    soc_cmicx_led_speed.

--- sdk-6.5.16/include/shared/cmicfw/m0_ver.h
+++ sdk-6.5.16/include/shared/cmicfw/m0_ver.h
@@ -6,5 +6,11 @@
  */
 
 /* define M0 FW version */
+/* 1.0 : Initial version support TD3, TH3, MV2, HX5.
+ * 1.1 : Debug version.
+ * 1.2 : SDK-166509: use share memory to speed up LED update.
+ */
+
 #define HOST_FW_VER_MAJOR  (0x1) /* 16 bit */
-#define HOST_FW_VER_MINOR  (0x0) /* 16 bit */
+#define HOST_FW_VER_MINOR  (0x2) /* 16 bit */
+
